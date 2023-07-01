7963b60bb890f9bdc595efb7214e26bccdb3079f
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-07-01 21:06:15 +0600

    Load All data

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('inside');
+const loadTools = async() =>{
+    try{
+
+        const res = await fetch(`https://openapi.programming-hero.co/api/ai/tools`);
+        const data = await res.json();
+        console.log(data);
+    }
+    catch(error){
+        alert("Sorry! Fixing some internal problems");
+        xonsole.log(error);
+        return 0;
+    }
+}
+
+loadTools();
