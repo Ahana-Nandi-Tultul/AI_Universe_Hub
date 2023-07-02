467d9331adf3b52e9cae8b56d2f1320e8de48ad6
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-07-02 10:56:17 +0600

    Showing all info of Tool Cards

--- js/index.js
+++ js/index.js
@@ -19,19 +19,53 @@ const displayTools = (data) =>{
         const toolCardDiv = document.createElement('div');
         toolCardDiv.classList.add('col');
         console.log(element);
-        toolCardDiv.innerHTML = `
-        <div class="card mb-3">
-            <img src="${element.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        const toolCard = document.createElement('div');
+        toolCard.classList.add("card", "mb-3", "p-3");
+        let toolcardBody = document.createElement('div');
+        toolcardBody.classList.add('card-body');
+        toolCard.innerHTML = `
+            <img src="${element.image}" class="card-img-top rounded" alt="...">
+        `;
+        toolcardBody.innerHTML = `
+        <h5 class="card-title">Features</h5>
+        `;
+        if(element.features){
+            displayFeatures(toolcardBody, element.features);
+        }
+        else(
+            toolcardBody.innerHTML += `<p class= "text-center">No Feature Found.</p>`
+        )
+        toolcardBody.innerHTML += `
+        <hr>
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <h5 class="card-title">${element.name}</h5>
+                <p><i class="fa fa-calendar-o" aria-hidden="true"></i> ${element.published_in?element.published_in: "Not Found"}</p>
+            </div>
+            <div>
+                <button class= "btn"><i class="fa fa-long-arrow-right text-danger" aria-hidden="true"></i></button>
             </div>
         </div>
         `;
+        toolCard.appendChild(toolcardBody)
+        toolCardDiv.appendChild(toolCard)
         toolsDiv.appendChild(toolCardDiv);
         
     });
+}
+
+const displayFeatures = (toolcardBody, features) =>{
+    const ol = document.createElement('ol');
+    ol.classList.add('ps-3');
+    ol.style.height = '72px';
+    features = features.slice(0, 3);
+    features.forEach(feature => {
+        console.log(feature);
+        ol.innerHTML += `
+        <li>${feature}</li>
+        `;
+    });
+    toolcardBody.appendChild(ol);
 }
 
 loadTools();
